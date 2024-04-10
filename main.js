772c2357a44543b5f1efb33d8b127adda0b4053e
mizu <psdk55052004@gmail.com>
2024-04-10 21:16:07 +0800

    新增 sessionStorage

--- main.js
+++ main.js
@@ -18,5 +18,6 @@ import './modules/constructor/newFormData.js';
 import './modules/constructor/newXMLHttpRequest.js';
 import './modules/constructor/newFileReader.js';
 import './modules/constructor/newPromise.js';
+import './modules/other/sessionStorage.js'
 
 
