2955daa4cd3d37994705d6d85c359e5837627292
mizu <psdk55052004@gmail.com>
2024-04-11 23:28:22 +0800

    新增 history

---
+++ modules/bom/history.js
@@ -0,0 +1,25 @@
+
+
+historyFn()
+
+function historyFn () {
+  // console.log(history)
+
+  // 會話的歷史紀錄數量
+  // console.log('history.length', history.length)
+
+  // 設定用戶捲動的位置是否會被恢復
+  // auto: 使用者捲動到的頁面位置將會恢復。
+  // manual: 頁面上的位置不會恢復。用戶必須手動滾動到該位置。
+  // history.scrollRestoration = 'auto'
+  // history.scrollRestoration = 'manual'
+
+  // 回上一頁
+  // history.back()
+
+  // 去下一頁
+  // history.forward()
+
+  // 到指定頁數的頁面
+  // history.go(-1)
+}
