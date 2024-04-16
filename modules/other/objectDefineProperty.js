feca30d4212317aba499d1a1152bea8cba9f180c
Mizu <psdk55052004@gmail.com>
2024-04-16 11:22:25 +0800

    新增 Object.defineProperty: value, writable, enumberable, configurable

--- modules/other/objectDefineProperty.js
+++ modules/other/objectDefineProperty.js
@@ -11,10 +11,52 @@ const person = {
     age: 3
   }
 }
+// 1. 基本架構
+// Object.defineProperty(person, 'name', {
+//   value: '', // 值
+//   writable: true, // 可否修改
+//   enumerable: true, // 可否列舉
+//   configurable: true // 可否刪除
+// })
+// console.log(person)
+
+
+// 2. value 值
 Object.defineProperty(person, 'name', {
-  value: '',
-  writable: true, // 可否修改
-  enumerable: true, // 可否列舉
-  configurable: true // 可否刪除
+  value: 'Jack'
 })
-console.log(person)
+// console.log(person.name) // Jack
+
+
+// 3. writable 可否修改
+Object.defineProperty(person, 'age', {
+  writable: false
+})
+// person.age = 30 // 報錯
+
+
+// 4. enumerable 可否列舉
+Object.defineProperty(person, 'hobby', {
+  enumerable: false
+})
+// console.log(Object.keys(person)) // ['name', 'age', 'cat']
+for (const key in person) {
+  // console.log('列舉',  key);   // 列舉: name, age, cat
+}
+
+
+// 5. configurable 可否刪除
+Object.defineProperty(person, 'cat', {
+  configurable: false
+})
+// delete person.cat // 報錯
+// delete person.name // 成功刪除
+
+
+// 6. 新增屬性
+Object.defineProperty(person, 'job', {
+  value: 'coder'
+})
+// console.log(person.job) // coder
+
+// Object.defineProperties() 
