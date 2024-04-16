e1c133ce304a79e9ea9000b43017d5a633408892
mizu <psdk55052004@gmail.com>
2024-04-16 19:11:55 +0800

    新增 navigator

---
+++ modules/bom/navigator.js
@@ -0,0 +1,37 @@
+
+
+
+navigatorFn()
+
+function navigatorFn () {
+  // console.log(navigator)
+
+  // 讀取已複製到剪貼簿的文字和圖片
+  // navigator.clipboard
+  // .readText()
+  // .then(clipText => console.log('clipText', clipText));
+
+  // 包含有關裝置網路連線資訊的物件, 例: 3g, 4g, wifi
+  // console.log(navigator.connection)
+
+  // 瀏覽器是否支援使用 cookie
+  // console.log(navigator.cookieEnabled)
+
+  // 回傳瀏覽器使用語言
+  // console.log(navigator.language) // zh-TW
+  // console.log(navigator.languages) // ['zh-TW', 'zh', 'en-US', 'en']
+
+  // 返回目前裝置支援的同時觸摸接觸點的最大數量。
+  // console.log(navigator.maxTouchPoints)
+
+
+  // 是否啟用 cookie
+  // console.log('cookieEnabled: ', navigator.cookieEnabled)
+
+  // 硬件平台
+  // console.log('platform: ', navigator.platform) // Win32
+
+  // 用戶代理: 瀏覽器名稱、版本、作業系統
+  // Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36
+  // console.log('userAgent: ', navigator.userAgent)
+}
