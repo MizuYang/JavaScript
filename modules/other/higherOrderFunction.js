0b4111bd9207d405f683ed85dc72290dd64efe06
Mizu <psdk55052004@gmail.com>
2024-04-18 11:06:02 +0800

    新增 高階函數: forEach

---
+++ modules/other/higherOrderFunction.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+// 高階函式 higherOrderFunction 
+
+// #region 高階函式 => forEach
+// const data = [1, 2, 3, 4]
+
+// Array.prototype.myForEach = function (callback) {
+//   // console.log(this) // [1, 2, 3, 4]
+
+//   // 整理出傳回去的 forEach 三個參數 (item, index, array)
+//   for (let i = 0; i < data.length; i++) {
+//     const item = data[i]
+//     // console.log(item) // 分別印出: 1, 2, 3, 4
+
+//     const index = i
+//     // console.log((index)) // 分別印出: 0, 1, 2, 3
+
+//     const array = this
+//     // console.log(array) // [1, 2, 3, 4]
+    
+//     // 將參數傳回去 callback 讓帶入的函式能夠使用
+//     callback(item, index, array)
+//   }
+// }
+
+// data.myForEach((item, index ,array) => {
+//   console.log('item', item);
+//   console.log('index', index);
+//   console.log('array', array);
+// })
+
+// #endregion
+
+
+
+
+
+
+
+
+
+
+
